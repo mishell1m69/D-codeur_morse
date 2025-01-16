b752267cf65214e5f4df0fadea6b0cc9b52c77f3
Clément MICHEL <159059762+mishell1m69@users.noreply.github.com>
2025-01-16 14:23:02 +0100

    Update scripts.js

--- web/scripts.js
+++ web/scripts.js
@@ -1,1 +1,9 @@
-
+// Utilisé pour afficher le menu lorsque l'écran est suffisament petit
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
