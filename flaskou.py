4c6ea8e84b983b8240ca8712c55725e13fece4b9
Jojo <157808588+Jojau12@users.noreply.github.com>
2025-01-21 13:20:54 +0100

    Add files via upload
    Made Flask works with every html pages

--- flaskou.py
+++ flaskou.py
@@ -8,9 +8,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/d_arbres')
+def d_arbres():
+    return render_template("d_arbres.html")
+
+@app.route('/d_dicos')
+def d_dicos():
+    return render_template("d_dicos.html")
+
 @app.route('/decoder')
 def decoder():
     return render_template("decoder.html")
+
+@app.route('/e_arbres')
+def e_arbres():
+    return render_template("e_arres.html")
+
+@app.route('/e_dicos')
+def e_dicos():
+    return render_template("e_dicos.html")
 
 @app.route('/encoder')
 def encoder():
